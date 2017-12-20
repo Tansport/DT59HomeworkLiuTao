d93e4b5f091506b80b67930716468af50f5f7536
lenovo <lenovo@DESKTOP-AHNC0OD>
2017-12-20 19:37:13 +0800

    add homework

---
+++ src/main/java/dt59homework/HelloWorld.java
@@ -0,0 +1,28 @@
+/**
+ * Project Name:dt59homework
+ * File Name:HelloWorld.java
+ * Package Name:dt59homework
+ * Date:2017年12月20日下午6:07:02
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package dt59homework;
+
+/**
+ * Description: <br/>
+ * Date: 2017年12月20日 下午6:07:02 <br/>
+ * 
+ * @author lenovo
+ * @version
+ * @see
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.err.println("HelloWorld");
+
+        // Auto-generated method stub
+
+    }
+
+}
