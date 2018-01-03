f2fbe1fffb194f23b8c029f70d3245c3ae12cd6c
lenovo <lenovo@DESKTOP-TAO>
2018-01-03 18:06:20 +0800

    Homework20180103

---
+++ src/main/java/dt59homework/hw20180103/Translator.java
@@ -0,0 +1,28 @@
+/**
+ * Project Name:dt59homework
+ * File Name:Translator.java
+ * Package Name:dt59homework.hw20180103
+ * Date:2018年1月3日下午2:56:38
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package dt59homework.hw20180103;
+
+/**
+ * Description: <br/>
+ * Date: 2018年1月3日 下午2:56:38 <br/>
+ * 
+ * @author liutao
+ * @version
+ * @see
+ */
+public interface Translator {// interface关键字建立接口的类
+    /**
+     * Description:翻译器方法 <br/>
+     *
+     * @author liutao
+     * @return
+     */
+    public String Information();// 信息方法，用public关键字,无方法体
+    // 有的接口里面什么方法都没有，只定义常量
+}
