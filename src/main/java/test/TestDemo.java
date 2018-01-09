44a19fd15defcb8041242ca4fee885a049c860e8
Administrator <Administrator@PC-201510181541>
2018-01-09 17:22:50 +0800

    test

---
+++ src/main/java/test/TestDemo.java
@@ -0,0 +1,27 @@
+/**
+ * Project Name:dt59homework
+ * File Name:TestDemo.java
+ * Package Name:test
+ * Date:2018年1月9日下午5:21:07
+ * Copyright (c) 2018, bluemobi All Rights Reserved.
+ */
+
+package test;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Description:   <br/>
+ * Date:     2018年1月9日 下午5:21:07 <br/>
+ * @author   liutao
+ * @version
+ * @see
+ */
+public class TestDemo {
+   private final static Logger LOG =Logger.getLogger(TestDemo.class);
+   public static void main(String[] args) {
+    LOG.info("Who am I");
+}
+
+}
+
