428efef27069b2ae371a59e50a33e81636f97e98
lenovo <lenovo@DESKTOP-TAO>
2018-01-10 11:23:03 +0800

    add

--- src/main/java/test/TestDemo.java
+++ src/main/java/test/TestDemo.java
@@ -11,17 +11,18 @@ package test;
 import org.apache.log4j.Logger;
 
 /**
- * Description:   <br/>
- * Date:     2018年1月9日 下午5:21:07 <br/>
- * @author   liutao
+ * Description: <br/>
+ * Date: 2018年1月9日 下午5:21:07 <br/>
+ * 
+ * @author liutao
  * @version
  * @see
  */
 public class TestDemo {
-   private final static Logger LOG =Logger.getLogger(TestDemo.class);
-   public static void main(String[] args) {
-    LOG.info("Who am I");
-}
+    private final static Logger LOG = Logger.getLogger(TestDemo.class);
+
+    public static void main(String[] args) {
+        LOG.info("Who am I");
+    }
 
 }
-
